5afa12a9e663e08e084bfacf37f615d6fa508b3e
HANDER DAVID GUILLEN ARTEAGA <hander.guillen.arteaga@estudiantes.cordillera.edu.ec>
2022-01-06 21:31:49 -0500

    cargar

--- Importarcsv/Importarcsv/Form1.Designer.cs
+++ Importarcsv/Importarcsv/Form1.Designer.cs
@@ -29,13 +29,48 @@ namespace Importarcsv
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tblProductos = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tblProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tblProductos
+            // 
+            this.tblProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblProductos.Location = new System.Drawing.Point(42, 64);
+            this.tblProductos.Name = "tblProductos";
+            this.tblProductos.Size = new System.Drawing.Size(700, 243);
+            this.tblProductos.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(158, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "IMPORTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tblProductos);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tblProductos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView tblProductos;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Importarcsv/Importarcsv/Form1.cs
+++ Importarcsv/Importarcsv/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Importarcsv
 {
@@ -15,6 +16,28 @@ namespace Importarcsv
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Filter = "Archivo CSV|*.csv" };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string SEP =",";
+
+                string[] lineas = File.ReadAllLines(ofd.FileName);
+                string[] cabeceras = lineas[0].Split(new[] { SEP }, StringSplitOptions.None);
+
+                tblProductos.Columns.Clear();
+                foreach (string c in cabeceras)
+                    tblProductos.Columns.Add(c, c);
+
+                for (int i = 1; i < lineas.Length; i++)
+                {
+                    string[] celdas = lineas[i].Split(new[] { SEP }, StringSplitOptions.None);
+                    tblProductos.Rows.Add(celdas);
+                }
+            }
         }
     }
 }
