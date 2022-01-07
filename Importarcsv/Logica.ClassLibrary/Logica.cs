23763a309ba68f7b99ef80750e9a13fdd2d77b94
HANDER DAVID GUILLEN ARTEAGA <hander.guillen.arteaga@estudiantes.cordillera.edu.ec>
2022-01-06 20:59:22 -0500

    avances

---
+++ Importarcsv/Logica.ClassLibrary/Logica.cs
@@ -0,0 +1,14 @@
+﻿using AccesoDatosClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.ClassLibrary
+{
+    public class Logica
+    {
+        private static DcProductosDataContext dc = new DcProductosDataContext();
+    }
+}
